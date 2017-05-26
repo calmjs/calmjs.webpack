6f37d87b2b5b03fb887c58612264eaba242e75ed
Tommy Yu <tommy.yu@auckland.ac.nz>
2017-05-26 16:40:59 +1200

    Initial boilerplate

---
+++ README.rst
@@ -0,0 +1,15 @@
+calmjs.webpack
+==============
+
+Package for extending the the `Calmjs framework`__ to support the usage
+of `webpack`__ for the generation of deployable artifacts from
+JavaScript source code provided by Python packages in conjunction with
+standard JavaScript or `Node.js`_ packages sourced from |npm|_ or other
+similar package repositories.
+
+.. __: https://pypi.python.org/pypi/calmjs
+.. __: https://webpack.js.org/
+
+.. |npm| replace:: ``npm``
+.. _Node.js: https://nodejs.org/
+.. _npm: https://www.npmjs.com/
---
+++ setup.py
@@ -0,0 +1,67 @@
+from setuptools import setup, find_packages
+
+version = '0.0'
+
+classifiers = """
+Development Status :: 3 - Alpha
+Intended Audience :: Developers
+License :: OSI Approved :: GNU General Public License v2 or later (GPLv2+)
+Operating System :: OS Independent
+Programming Language :: JavaScript
+Programming Language :: Python :: 2.7
+Programming Language :: Python :: 3.3
+Programming Language :: Python :: 3.4
+Programming Language :: Python :: 3.5
+Programming Language :: Python :: 3.6
+""".strip().splitlines()
+
+package_json = {
+    "dependencies": {},
+    "devDependencies": {
+        "webpack": "~2.6.0",
+    }
+}
+
+long_description = (
+    open('README.rst').read()
+    + '\n' +
+    open('CHANGES.rst').read()
+    + '\n')
+
+setup(
+    name='calmjs.webpack',
+    version=version,
+    description=(
+        "Package for extending the Calmjs framework to support the usage of "
+        "webpack for the generation of deployable artifacts from "
+        "JavaScript source code provided by Python packages in "
+        "conjunction with standard JavaScript or Node.js packages sourced "
+        "from npm or other similar package repositories."
+    ),
+    long_description=long_description,
+    classifiers=classifiers,
+    keywords='',
+    author='Tommy Yu',
+    author_email='tommy.yu@auckland.ac.nz',
+    url='https://github.com/calmjs/calmjs.webpack',
+    license='gpl',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    namespace_packages=['calmjs'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'calmjs>=2.0.0,<3',
+        'slimit>=0.8.0',
+    ],
+    extras_require={
+        'dev': [
+            'calmjs.dev>=1.0.2,<2',
+        ],
+    },
+    entry_points={
+    },
+    package_json=package_json,
+    calmjs_module_registry=['calmjs.module'],
+    test_suite="calmjs.webpack.tests.make_suite",
+)
